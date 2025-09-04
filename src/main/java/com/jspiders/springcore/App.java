9834140ff704f76f9dd8d81f31a2117b4161cee5
yuva <yuvaprasath444@gmail.com>
2025-09-04 17:24:15 +0530

    third commit

--- src/main/java/com/jspiders/springcore/App.java
+++ src/main/java/com/jspiders/springcore/App.java
@@ -5,6 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.jspiders.springcore.dto.StudentDto;
+import com.jspiders.springcore.dto.mobile;
 //import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class App {
@@ -12,6 +13,9 @@ public class App {
 		ApplicationContext context=new ClassPathXmlApplicationContext("META-INF/beans.xml");
 		System.out.println("IoC CONTAINER CREATED!!!");
 		System.out.println(context.getBean(StudentDto.class));
+		System.out.println(context.getBean("student2",StudentDto.class));
+		System.out.println(context.getBean(mobile.class));
+		System.out.println(context.getBean("mobile2",mobile.class));
 //		context=new FileSystemXmlApplicationContext("xml-file");
 //		context=new AnnotationConfigApplicationContext("xml-file");
 	}
---
+++ src/main/java/com/jspiders/springcore/dto/mobile.java
@@ -0,0 +1,12 @@
+package com.jspiders.springcore.dto;
+
+import lombok.Data;
+
+@Data
+public class mobile {
+	private int MobileID;
+	private String Brand_Name;
+	private String Mobile_Name;
+	private String Color;
+	private double price;
+}
